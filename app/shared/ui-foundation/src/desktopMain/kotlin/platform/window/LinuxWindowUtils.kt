56655dd4d6ba95da7deb00850e7f90fe3f8845dd
ChiveHao <chivehao@ikaros.run>
2025-08-10 19:36:33 +0800

    feat: switch fullscreen in linux such as ubuntu gnome (#2382)

--- app/shared/ui-foundation/src/desktopMain/kotlin/platform/window/LinuxWindowUtils.kt
+++ app/shared/ui-foundation/src/desktopMain/kotlin/platform/window/LinuxWindowUtils.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 OpenAni and contributors.
+ * Copyright (C) 2024-2025 OpenAni and contributors.
  *
  * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
  * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
@@ -9,4 +9,20 @@
 
 package me.him188.ani.app.platform.window
 
-class LinuxWindowUtils : AwtWindowUtils() 
+import androidx.compose.ui.window.WindowPlacement
+import androidx.compose.ui.window.WindowState
+import me.him188.ani.app.platform.PlatformWindow
+
+class LinuxWindowUtils : AwtWindowUtils() {
+    override suspend fun setUndecoratedFullscreen(
+        window: PlatformWindow,
+        windowState: WindowState,
+        undecorated: Boolean
+    ) {
+        if (undecorated) {
+            windowState.apply { placement = WindowPlacement.Fullscreen }
+        } else {
+            windowState.apply { placement = WindowPlacement.Floating }
+        }
+    }
+}
