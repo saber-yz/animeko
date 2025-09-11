99867157ef6f54deeb0163468bd8b4864f7442b5
Him188 <Him188@mamoe.net>
2025-09-11 15:51:02 +0100

    OpenAPI generator: automatically fix uploadAvatar

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -120,10 +120,24 @@ val fixGeneratedOpenApi = tasks.register("fixGeneratedOpenApi") {
 
     doLast {
         outputDir.walk().filter { it.isFile }.forEach {
-            val text = it.readText()
+            var text = it.readText()
+            val original = text
+            text = text.replace(
+                """
+                    open suspend fun uploadAvatar(body: me.him188.ani.client.infrastructure.OctetByteArray): HttpResponse<kotlin.Any> {
+                """.trimIndent(),
+                """
+                    open suspend fun uploadAvatar(body: io.ktor.http.content.OutgoingContent): HttpResponse<kotlin.Any> {
+                """.trimIndent()
+            )
+
             val off = "off" // 防止 IDE 把我们这个代码识别成指令
             if (!text.contains("// @formatter:$off")) {
-                it.writeText("// @formatter:$off\n$text\n// @formatter:on\n")
+                text = "// @formatter:$off\n$text\n// @formatter:on\n"
+            }
+
+            if (text != original) {
+                it.writeText(text)
             }
         }
     }
