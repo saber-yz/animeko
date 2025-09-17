3bd12c15f967348395cb77057ccf3609306339fb
KAMOsama <99065946+KAMO030@users.noreply.github.com>
2025-09-18 02:55:22 +0800

    fix(ios): 修复ios声音无法在静音模式下正常播放与屏幕亮度无法正常设置问题 (#2451)
    * feat(ios): 实现 IosAudioManager，支持获取和设置系统音量
    
    - 实现 `IosAudioManager`，支持获取和设置系统音量
    - 通过 `MPVolumeView` 和 `UISlider` 操作音量
    - 添加防抖逻辑，在 3 秒后自动移除 `MPVolumeView`
    
    * feat(ios): 配置音频会话以支持在静音模式下播放声音
    
    - 设置 `AVAudioSession` 的类别为 `Playback`，支持后台音频播放
    - 激活音频会话
    
    * fix(ios): 修复 IosBrightnessManager 屏幕亮度无法正常设置问题
    
    - 添加临时变量存储屏幕亮度，解决 UIScreen.mainScreen.brightness 设置后无法立即生效的问题
    - 调整 setBrightness 方法，实现对亮度值的范围约束
    
    * feat(ios): 监听屏幕亮度变化事件并同步更新亮度值
    
    - 使用 `NSNotificationCenter` 监听 `UIScreenBrightnessDidChangeNotification`
    - 自动更新临时变量 `brightness` 保存最新的屏幕亮度值
    
    * refactor(ios): 调整音频会话配置位置
    
    - 移除 AniIos.kt 中的音频会话配置代码
    - 将音频会话配置移到 IosAudioManager 初始化中- 优化代码结构，提高音频管理相关代码的集中度
    
    * fix(ios): 修正 setBrightness 方法中变量命名拼写错误
    
    - 将变量 `salvedLevel` 更正为 `savedLevel`，提高代码可读性

--- app/shared/app-platform/src/iosMain/kotlin/platform/features/PlatformComponentAccessors.ios.kt
+++ app/shared/app-platform/src/iosMain/kotlin/platform/features/PlatformComponentAccessors.ios.kt
@@ -9,23 +9,100 @@
 
 package me.him188.ani.app.platform.features
 
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import me.him188.ani.app.platform.Context
+import me.him188.ani.app.tools.MonoTasker
+import platform.AVFAudio.AVAudioSession
+import platform.AVFAudio.AVAudioSessionCategoryPlayback
+import platform.AVFAudio.setActive
+import platform.CoreGraphics.CGRectMake
+import platform.Foundation.NSNotificationCenter
+import platform.MediaPlayer.MPVolumeView
+import platform.UIKit.UIApplication
 import platform.UIKit.UIScreen
+import platform.UIKit.UIScreenBrightnessDidChangeNotification
+import platform.UIKit.UISlider
+import platform.darwin.dispatch_async
+import platform.darwin.dispatch_get_main_queue
+
 
 actual fun getComponentAccessorsImpl(context: Context): PlatformComponentAccessors =
     IosPlatformComponentAccessors
 
 private object IosPlatformComponentAccessors : PlatformComponentAccessors {
-    override val audioManager: AudioManager? get() = null // Ios does not allow adjusting system volume.
+    override val audioManager: AudioManager? by lazy { IosAudioManager() }
     override val brightnessManager: BrightnessManager? by lazy { IosBrightnessManager() }
 }
 
 private class IosBrightnessManager : BrightnessManager {
-    override fun getBrightness(): Float {
-        return UIScreen.mainScreen.brightness.toFloat()
+    /**
+     * 屏幕亮度
+     *
+     *  UIScreen.mainScreen.brightness 设置后不能马上生效所以get的时候需要一个临时值
+     */
+    var brightness = UIScreen.mainScreen.brightness.toFloat()
+
+    init {
+        NSNotificationCenter.defaultCenter.addObserverForName(
+            name = UIScreenBrightnessDidChangeNotification,
+            `object` = null,
+            queue = null,
+        ) { _ ->
+            brightness = UIScreen.mainScreen.brightness.toFloat()
+        }
     }
 
-    override fun setBrightness(level: Float) {
-        UIScreen.mainScreen.brightness = level.coerceIn(0f, 1f).toDouble()
+    override fun getBrightness(): Float = brightness
+
+    override fun setBrightness(level: Float) =
+        level.coerceIn(0f, 1f).let { savedLevel ->
+            brightness = savedLevel
+            UIScreen.mainScreen.brightness = savedLevel.toDouble()
+        }
+}
+
+@OptIn(ExperimentalForeignApi::class)
+internal class IosAudioManager : AudioManager {
+
+    @OptIn(ExperimentalForeignApi::class)
+    val volumeView = MPVolumeView(CGRectMake(-2000.0, -2000.0, 0.0, 0.0))
+
+    val tasker = MonoTasker(CoroutineScope(Dispatchers.Main))
+
+    val slider by lazy {
+        // 在 MPVolumeView 中找到 UISlider 组件
+        volumeView.subviews.first { it is UISlider } as UISlider
     }
+
+    init {
+        AVAudioSession.sharedInstance().run {
+            setCategory(AVAudioSessionCategoryPlayback, null)
+            setActive(true, null)
+        }
+    }
+
+    override fun getVolume(streamType: StreamType): Float {
+        return slider.value
+    }
+
+    @OptIn(ExperimentalForeignApi::class)
+    override fun setVolume(streamType: StreamType, levelPercentage: Float) {
+        if (volumeView.superview == null) {
+            UIApplication.sharedApplication.keyWindow?.addSubview(volumeView)
+        }
+        dispatch_async(queue = dispatch_get_main_queue()) {
+            // 设置音量
+            slider.setValue(levelPercentage.coerceIn(0f, 1f), animated = false)
+        }
+        // 防抖移除 MPVolumeView
+        tasker.launch {
+            delay(3000)
+            // 移除 MPVolumeView
+            volumeView.removeFromSuperview()
+        }
+    }
+
 }
